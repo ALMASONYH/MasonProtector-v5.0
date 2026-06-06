6538327c737176cc1feeff273772c7324b374f52
Freemasonry <117776155+ALMASONYH@users.noreply.github.com>
2026-06-06 17:56:14 +0300

    ByFreemasonry
    MasonGroup

---
+++ MasonProtector/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mason Protector")]
+[assembly: AssemblyDescription(".NET Assembly Protector by MasonGroup (Freemasonry)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MasonGroup")]
+[assembly: AssemblyProduct("Mason Protector")]
+[assembly: AssemblyCopyright("Copyright © MasonGroup (Freemasonry) 2025 — Battal Alqhtani & Turki Alotibi")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1cff31bc-191c-4a1f-b53a-30ed609a7ed0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
---
+++ MasonProtector/Properties/Resources.Designer.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace MasonProtector.Properties {
+    using System;
+
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    public class Resources {
+
+        private static global::System.Resources.ResourceManager resourceMan;
+
+        private static global::System.Globalization.CultureInfo resourceCulture;
+
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal Resources() {
+        }
+
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        public static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MasonProtector.Properties.Resources", typeof(Resources).Assembly);
+                    resourceMan = temp;
+                }
+                return resourceMan;
+            }
+        }
+
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        public static global::System.Globalization.CultureInfo Culture {
+            get {
+                return resourceCulture;
+            }
+            set {
+                resourceCulture = value;
+            }
+        }
+
+        public static string freemasonry {
+            get {
+                return ResourceManager.GetString("freemasonry", resourceCulture);
+            }
+        }
+    }
+}
